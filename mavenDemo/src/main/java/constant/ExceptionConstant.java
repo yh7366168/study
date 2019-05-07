5319ba713b9f86e9db871f3ba708232ac2144d02
yh7366168 <2769952764@qq.com>
2019-05-07 14:16:05 +0800

    push测试

--- mavenDemo/src/main/java/constant/ExceptionConstant.java
+++ mavenDemo/src/main/java/constant/ExceptionConstant.java
@@ -3,7 +3,9 @@ package constant;
 public class ExceptionConstant {
 
     public static class TestConstant{
-        public static String ExcceptionMessage = "测试异常";
+        public static String ExcceptionMessage_1 = "测试异常1";
+
+        public static String ExcceptionMessage_2 = "测试异常2";
     }
 
 
